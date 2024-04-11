e11814b4b6bf31b0c86260232c8902489118059f
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-04-11 10:00:47 +0200

    Add Calculator class and associated tests
    Two new files were created: Calculator.java and CalculatorTest.java, introducing basic arithmetic operations and associated unit tests respectively. The MainActivity file was also modified by removing unnecessary print statements.

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Calculator {
+    private static final String ZERO_DIVIDER_ERROR = "Divider can't be zero";
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException(ZERO_DIVIDER_ERROR);
+        }
+        return a / b;
+    }
+}
